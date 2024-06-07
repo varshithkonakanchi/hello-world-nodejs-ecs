f33e215c1383bae3bd8d51dc6e46d265cbc2f0d0
varshithkonakanchi <103957324+varshithkonakanchi@users.noreply.github.com>
2024-06-07 10:29:51 +0530

    Add files via upload

---
+++ terraform/network.tf
@@ -0,0 +1,27 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "default" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
